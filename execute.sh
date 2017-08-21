d25ecc54b1b46e439c88438f649c2acf1159eb3e
Niranjan <niranjan@concret.io>
2017-08-21 15:46:25 +0530

    Commit 21.08.2017

---
+++ execute.sh
@@ -0,0 +1,20 @@
+echo "--------------------------------------------------"
+echo "Current Directory listing"
+echo "--------------------------------------------------"
+
+ls
+echo "--------------------------------------------------"
+echo "Current Directory Path"
+echo "--------------------------------------------------"
+
+pwd
+
+echo "--------------------------------------------------"
+echo "Fired the core command"
+echo "--------------------------------------------------"
+
+<<<<<<< HEAD
+java -cp ".:./lib/*:./dist/*" org.testng.TestNG testng.xml
+=======
+java -cp ".:./lib/*:./DIST/*" org.testng.TestNG testng.xml
+>>>>>>> f634ccc1e87873979fcf7fdd282df1733d05b615
